e69d7c31dbaabbf332d159cdfdcaa52cafd9ba7c
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2024-12-06 19:53:26 +0530

    asfd

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 const Feed = () => {
   return (
-    <div className="flex flex-col pt-1 px-4">
+    <div className="flex flex-col pt-1 px-4 overflow-y-scroll h-[90vh] scroll-smooth ">
       <Card />
     </div>
   );
