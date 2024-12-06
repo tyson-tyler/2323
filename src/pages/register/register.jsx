b3e873f0b3f8f3c3e62e7e225c8ae7b102c8e57d
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2024-12-06 16:11:27 +0530

    asdf

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -68,7 +68,7 @@ const Register = () => {
               <label className="text-sm font-semibold">Name</label>
               <input
                 type="text"
-                className="flex h-12 outline-none w-full rounded-md border  bg-gray-800 text-white px-3 py-2 text-sm"
+                className="flex h-12 outline-none w-full rounded-md  bg-gray-800 text-white px-3 py-2 text-sm"
               />
             </div>
             <div className="space-y-2">
@@ -78,7 +78,7 @@ const Register = () => {
                 required
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="flex h-12 outline-none w-full rounded-md border  bg-gray-800 text-white px-3 py-2 text-sm"
+                className="flex h-12 outline-none w-full rounded-md   bg-gray-800 text-white px-3 py-2 text-sm"
               />
             </div>
             <div className="space-y-2">
